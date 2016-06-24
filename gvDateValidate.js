1e362cb427a747100248bb81ee72339304e32457
george-viaud <george@mediaforce.com>
2016-06-24 08:36:46 -0700

    Initial commit

---
+++ gvDateValidate.js
@@ -0,0 +1,10 @@
+function isValidDate(y, m, d)
+{
+  y = parseInt(y, 10);
+  m = parseInt(m, 10) - 1;
+  d = parseInt(d, 10);
+  
+  dateObj = new Date(y, m, d);
+  
+  return (d == dateObj.getDate() && m == dateObj.getMonth() && y == dateObj.getFullYear());
+}
